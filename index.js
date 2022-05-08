fa8a5064817a2e3f3c312687b9873cc5d4119796
MaxMady <81635616+MaxMady@users.noreply.github.com>
2022-05-08 06:39:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+const { Client, MessageEmbed, MessageActionRow, MessageButton } = require('discord.js')
+const client = new Client({ intents: 7753 })
+
+const disboard = require('./disboard.js')
+
+console.log(`Logging in...`)
+client.on('ready', async () => {
+  console.log(`${client.user.tag} is ready to roll!`)
+  
+  disboard()
+});
+
+client.login(`token`)
