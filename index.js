657f0d529774258833d85f4fefd996fd87d8ba96
MaxMady <81635616+MaxMady@users.noreply.github.com>
2022-05-09 06:53:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const { Client, MessageEmbed, MessageActionRow, MessageButton } = require('disco
 const client = new Client({ intents: 7753 })
 
 const disboard = require('./disboard.js')
+const anigame = require('./anigame.js')
 
 console.log(`Logging in...`)
+
 client.on('ready', async () => {
   console.log(`${client.user.tag} is ready to roll!`)
   
   disboard()
 });
 
+client.on('messageCreate', async message => {
+  anigame(message);
+})
 client.login(`token`)
